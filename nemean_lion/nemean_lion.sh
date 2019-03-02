6f3254cc2afebd55b57789bcd8471022d3c1cc4c
Alexander Carlson <acarlson@e1z2r3p26.42.us.org>
2019-03-01 22:51:12 -0800

    Sort of fixed

--- nemean_lion/nemean_lion.sh
+++ nemean_lion/nemean_lion.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-sed 's/#Port 22/Port 50000/' /etc/ssh/sshd_config > sshd_config.tmp
+sed 's/#Port .*/Port 50000/' /etc/ssh/sshd_config > sshd_config.tmp
 mv sshd_config.tmp /etc/ssh/sshd_config
 service sshd restart
