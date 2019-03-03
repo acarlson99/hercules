d10e373ce9b6bfc1857067bc59512d499cfd0d3b
root <root@debian.domainname>
2019-03-03 18:27:14 -0500

    Fix

--- nemean_lion/nemean_lion.sh
+++ nemean_lion/nemean_lion.sh
@@ -6,13 +6,13 @@ then
 	exit 1
 fi
 
+apt-get install openssh-server -y
+
 if [ ! -f /etc/ssh/sshd_config ]
 then
 	echo "File not found"
 	exit 1
 fi
-
-apt-get install openssh-server -y
 
 sed 's/^#\? \?Port .*/Port 50000/' /etc/ssh/sshd_config > sshd_config.tmp
 
