bd56e5314aa99d8f522bed7c96c547c3e515b0b7
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-03-07 16:21:34 -0800

    Edited some stuff

--- lernaean_hydra/main.c
+++ lernaean_hydra/main.c
@@ -6,7 +6,7 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 11:31:10 by acarlson          #+#    #+#             */
-/*   Updated: 2019/03/07 14:29:44 by acarlson         ###   ########.fr       */
+/*   Updated: 2019/03/07 16:21:21 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,10 @@ int			create_socket(void)
 	return (s);
 }
 
-void		listen_loop(int pid, int sid, int d)
+void		listen_loop(struct sockaddr_in *server, struct sockaddr_in *client)
 {
-	(void)pid;
-	(void)sid;
-	(void)d;
+	(void)server;
+	(void)client;
 	while (1);
 }
 
@@ -35,8 +34,10 @@ int			main(int argc, char **argv)
 	pid_t					sid;
 	int						d;
 	struct sockaddr_in		server;
+	struct sockaddr_in		client;
 
 	(void)server;
+	(void)client;
 	if ((d = read_args(argc, argv)) < 0)
 		exit(1);
 	pid = 0;
@@ -55,5 +56,5 @@ int			main(int argc, char **argv)
 	// close(STDIN_FILENO);
 	// close(STDOUT_FILENO);
 	// close(STDERR_FILENO);
-	listen_loop(pid, sid, d);
+	listen_loop(&server, &client);
 }
