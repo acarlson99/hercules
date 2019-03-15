7f3887daf8b3cf589e74e9b44a9d7bbdc7f8e865
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-03-15 16:03:40 -0700

    Added oauth tokens :)

--- ceryneian_hind/main.rb
+++ ceryneian_hind/main.rb
@@ -2,7 +2,20 @@
 
 require 'oauth2'
 
+UID = # NOT PUSHING THIS
+SECRET = # NOT PUSHING THIS
+
+def findBoy(token, boy)
+
+end
+
 def main()
+  client = OAuth2::Client.new(UID, SECRET, site: "https://api.intra.42.fr")
+  token = client.client_credentials.get_token
+
+  File.foreach("boys.txt").with_index do |line, line_num|
+    puts "#{line_num}: #{line[0..-2]} #{findBoy(token, line)}"
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
