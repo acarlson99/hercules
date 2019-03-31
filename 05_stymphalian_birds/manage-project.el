3e8120fa8ff0d31803b7dd7cc2c427da52d73da9
Alexander Carlson <acarlson@e1z3r2p18.42.us.org>
2019-03-31 14:17:52 -0700

    Fixed clone functions

--- 05_stymphalian_birds/manage-project.el
+++ 05_stymphalian_birds/manage-project.el
@@ -1,19 +1,27 @@
 ;;; package --- Summary
 ;;; Commentary:
 ;;; Code:
+
 (defun clone-gitignore ()
   "Clone gitignore template."
-  (magit-clone "https://github.com/bombblob/gitignore_template.git" "gitignore_dir")
-  (rename-file "gitignore_dir/.gitignore" "./.gitignore")
-  (delete-directory "gitignore_dir")
-  )
-
-;; this is broken :(
+  (if (file-exists-p ".gitignore")
+	  (message ".gitignore file exists.  Skipping clone-gitignore")
+	(progn
+	  (message "cloning gitignore")
+	  (shell-command "git clone https://github.com/bombblob/gitignore_template.git gitignore_dir")
+	  (rename-file "gitignore_dir/.gitignore" "./.gitignore")
+	  (shell-command "rm -rf gitignore_dir")
+	  )))
 
 (defun clone-libft ()
   "Clone libft."
-  (magit-clone "https://github.com/bombblob/libft.git")
-  )
+  (if (file-exists-p "libft")
+	  (message "libft exists.  Skipping clone-libft")
+	(progn
+	  (message "cloning libft")
+	  (shell-command "git clone https://github.com/bombblob/libft.git libft")
+	  (shell-command "rm -rf libft/.git")
+	  )))
 
 (defun make-author ()
   "Create author file."
