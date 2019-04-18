09d504fcac83f431d202ff779507205918e05441
Alexander Carlson <acarlson@e1z3r2p18.42.us.org>
2019-04-18 14:46:46 -0700

    yeet

---
+++ 08_girdle_of_hippolyta/main.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+import yagmail
+import os
+import sys
+import base64
+
+def main():
+    if len(sys.argv) != 3:
+        print("usage: ./main.py person@example.com subject")
+        sys.exit(1)
+    try:
+        try:
+            FROM = os.environ['EMAIL_FROM']
+            FROM_PASS = os.environ['EMAIL_FROM_PASS']
+            TO = sys.argv[1]
+            SUBJECT = sys.argv[2]
+        except:
+            print("Make sure EMAIL_FROM and EMAIL_FROM_PASS environment variables are set")
+        sys.stdout.write('> ')
+        sys.stdout.flush()
+        MSG = ""
+        for line in sys.stdin:
+            MSG += line
+            sys.stdout.write('> ')
+            sys.stdout.flush()
+        yagmail.SMTP(FROM, FROM_PASS).send(TO, SUBJECT, MSG)
+    except Exception as ex:
+        print("Error sending message")
+        print(ex)
+
+if __name__ == "__main__":
+    main()
