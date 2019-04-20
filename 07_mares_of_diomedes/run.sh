f81c7741145ef906ea4da629702cbfd7097f7142
Alexander Carlson <acarlson@e1z3r2p8.42.us.org>
2019-04-20 15:57:39 -0700

    yee

--- 07_mares_of_diomedes/run.sh
+++ 07_mares_of_diomedes/run.sh
@@ -1,12 +1,7 @@
 ./manger &
 
-sleep 15
+sleep 30
 
-# while [[  $(killall -11 lampon; killall -11 ruins; killall -11 -- -bash; killall -11 yes; killall -11 manger; rm -f *.poo; pkill -11 'poo';) ]]
-# do
-# 	:;
-# done
-
-kill -s KILL $(pgrep crap lampon ruins xanthos yes)
-kill -s KILL $(top -l 1 | grep manger | awk '{print $1}')
+kill -11 $(pgrep lampon ruins xanthos yes poo)
+kill -11 $(top -l1 | grep manger | awk '{print $1}')
 rm -f *.poo
