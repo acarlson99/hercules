2348a892955223497f19bb309b0f14085225af67
Alexander <alexandercarlson99@gmail.com>
2019-05-03 21:45:29 -0700

    All left is to log to file

--- 09_cattle_of_geryon/main.go
+++ 09_cattle_of_geryon/main.go
@@ -1,30 +1,127 @@
 package main
 
 import (
-	"fmt";
-	"net/http";
-	"flag";
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+
+	"flag"
+
+	"github.com/schollz/progressbar"
 )
 
-// channel for responses
-// channel for iterating through requests
+// channel for responses and times
 // func for sending requests
 // func for logging all responses
 // func to sum responses
 
+type Response struct {
+	code int
+	time float64
+}
+
+// usage: ./main 'http://google.com' 100
+
+// func main() {
+// 	fmt.Println("HI")
+
+// 	flag.Parse()
+// 	args := flag.Args()
+
+// 	if len(args) != 2 {
+// 		fmt.Println("usage: ./main 'http://example.com' num_requests")
+// 		return
+// 	}
+// 	resp, err := http.Get(args[0])
+// 	if err != nil {
+// 		fmt.Println("Error requesting from", args[0])
+// 		return
+// 	}
+
+// 	fmt.Println(Response{1, 100})
+
+// 	fmt.Println(resp.StatusCode)
+// 	defer resp.Body.Close()
+
+// 	fmt.Printf("%d\n", len(args))
+// 	fmt.Println(args[0])
+// }
+
+func logErr(err error) {
+	fmt.Println(err) // TODO: actually log error.  Like in a file
+}
+
+func sendRequest(server string, wg *sync.WaitGroup, ch chan Response, gen chan int) {
+	defer wg.Done()
+
+	for i := <-gen; i > 0; i = <-gen {
+		begin := time.Now()
+		resp, err := http.Get(server)
+		if err != nil {
+			logErr(err)
+			ch <- Response{-1, -1}
+		} else {
+			r := Response{resp.StatusCode, time.Since(begin).Seconds()}
+			ch <- r
+		}
+	}
+}
+
+func logToFile(numRequests int, wg *sync.WaitGroup, ch chan Response) {
+	defer wg.Done()
+
+	bar := progressbar.New(numRequests)
+
+	for i := 0; i < numRequests; i++ {
+		_ = <-ch
+		// fmt.Println(rq)
+		bar.Add(1)
+	}
+}
+
+func generator(numRequests int, gen chan int) {
+	for i := numRequests; i > 0; i-- {
+		gen <- i
+		time.Sleep(5)
+	}
+	for {
+		gen <- 0
+	}
+}
+
 func main() {
-	fmt.Println("HI")
-	resp, err := http.Get("https://google.com")
-	if err != nil {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 2 {
+		fmt.Println("usage: ./main 'http://example.com' num_requests")
 		return
 	}
 
-	flag.Parse()
-	args := flag.Args()
+	numRequests, err := strconv.Atoi(args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	fmt.Println(resp.StatusCode)
-	defer resp.Body.Close()
+	var wg sync.WaitGroup
 
-	fmt.Printf("%d\n", len(args))
-	fmt.Println(args[0])
+	numRoutines := 4
+
+	wg.Add(numRoutines + 1)
+
+	ch := make(chan Response)
+	gen := make(chan int)
+
+	go generator(numRequests, gen)
+
+	go logToFile(numRequests, &wg, ch)
+
+	for i := 0; i < numRoutines; i++ {
+		go sendRequest(args[0], &wg, ch, gen)
+	}
+
+	wg.Wait()
+	fmt.Println("")
 }
